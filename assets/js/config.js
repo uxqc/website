9773b8f937a83e9b6b6dee5f2c2bf8bfe05a5508
uxqc <uxqc69@gmail.com>
2024-08-08 01:18:21 +0200

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,39 @@
+const BADGES = [
+  {
+    name: "HypeSquad Bravery", // Message that will appear when hovering over the badge
+    image: "assets/badges/hypesquad_bravery.svg",
+  },
+  {
+    name: "Subscriber since Jun 29, 2022",
+    image: "assets/badges/nitro.svg",
+  },
+  {
+    name: "Server boosting Jun 29, 2022",
+    image: "assets/badges/boost.svg",
+  },
+  {
+    name: "Otkrio si tajnu opciju!", // Message that appears when you hover over it
+    image: "assets/badges/bug_hunter_level2.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+];
+
+const USER_ID = "611495801843089420"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const BACKGROUND_IMAGE = "assets/images/star.gif"; // Here is the background of the site, behind your profile
+const STATUS = {
+  dnd: {
+    name: "Do not disturb", // Discord Status
+    color: "#ed4245",
+  },
+  idle: {
+    name: "Busy",
+    color: "#faa81a",
+  },
+  online: {
+    name: "Online",
+    color: "#3ba55d",
+  },
+  offline: {
+    name: "Offline",
+    color: "#6e7985",
+  },
+};
